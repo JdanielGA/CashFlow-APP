a0d6eccd8e31e294a49a6fbcc8ee1fdb0340f1c9
Daniel Granada <j.daniel_granada@hotmail.com>
2023-11-04 15:00:31 -0500

    Create feacture added for the Cash Flow section

--- routers/cash_flow.py
+++ routers/cash_flow.py
@@ -27,3 +27,20 @@ def get_all_cash_flow_records():
         return JSONResponse(status_code=500, content={'message': f'Error: {e}.'})
     except:
         raise HTTPException(status_code=500, detail='Internal server error.')
+    
+# Desc: Function to create a Cash Flow record calling the service class.
+@cash_flow_router.post('/cash_flow/create', tags=['Cash Flow'], response_model=CashFlowSchema)
+def create_new_record(cash_flow: CashFlowSchema = Depends()):
+    try:
+        db = SessionLocal()
+        check_ref_number = CashFlowServices(db).get_by_ref_number(cash_flow.ref_number)
+        if check_ref_number:
+            return JSONResponse(status_code=400, content={'message': 'The reference number already exists in the database.'})
+        new_record = CashFlowServices(db).create(cash_flow)
+        return JSONResponse(status_code=201, content={'message': 'The new record was created successfully.'})
+    except Exception as e:
+        return JSONResponse(status_code=500, content={'message': f'Error: {e}.'})
+    except:
+        raise HTTPException(status_code=500, detail='Internal server error.')
+
+        
--- services/cash_flow.py
+++ services/cash_flow.py
@@ -14,3 +14,15 @@ class CashFlowServices:
     def get_all(self):
         records = self.db.query(CashFlowModel).all()
         return records
+    
+    def get_by_ref_number(self, ref_number: str):
+        record = self.db.query(CashFlowModel).filter(CashFlowModel.ref_number == ref_number).first()
+        return record
+    
+    # Desc: Function to create a Cash Flow record.
+    def create(self, cash_flow: CashFlowSchema):
+        new_record = CashFlowModel(**cash_flow.model_dump())
+        self.db.add(new_record)
+        self.db.commit()
+        self.db.refresh(new_record)
+        return new_record
